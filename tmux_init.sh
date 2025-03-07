df6db2387edfa947fbe4cdbfac075e4c88d680e9
Alex Benn <alexbenn@microsoft.com>
2025-03-07 13:33:27 -0500

    dont do tmux out of tha box

--- tmux_init.sh
+++ tmux_init.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-SESSIONNAME="happy-session"
-WINDOWNAME="happy-window"
-tmux has-session -t $SESSIONNAME &> /dev/null
+happy_tmux () {
+    SESSIONNAME="happy-session"
+    WINDOWNAME="happy-window"
+    tmux has-session -t $SESSIONNAME &> /dev/null
 
-if [ $? != 0 ]
-then
-    tmux new-session -s $SESSIONNAME -n $WINDOWNAME -c $HOME -d
-    tmux set-option -t $SESSIONNAME visual-bell both
-fi
+    if [ $? != 0 ]
+    then
+	tmux new-session -s $SESSIONNAME -n $WINDOWNAME -c $HOME -d
+	tmux set-option -t $SESSIONNAME visual-bell both
+    fi
 
-tmux attach -t $SESSIONNAME
+    tmux attach -t $SESSIONNAME
+}
