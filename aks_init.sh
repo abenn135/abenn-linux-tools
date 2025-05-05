82921a8d6119aa6f62c364c2bfff141bf9ad1a80
Alex Benn <alexbenn@microsoft.com>
2025-05-05 16:03:41 +0000

    fix rproot, eval path at call time

--- aks_init.sh
+++ aks_init.sh
@@ -1,1 +1,5 @@
-alias rproot="cd \"$( pwd | sed -E 's/(.*aks/rp).*/\1/' )\""
+rproot () {
+    # This function changes the current directory to the root of the AKS RP repository.
+    # It uses 'sed' to extract the path up to 'aks/rp' and then changes to that child directory.
+    cd "$( pwd | sed -E 's/(.*aks\/rp).*/\1/' )"
+}
