847fe4bd02453c0aff78a1b6fa0b2bd1dd8b0f63
Alex Benn <alex.benn.is@gmail.com>
2025-10-17 11:00:12 -0400

    add falcon stuff

--- bashrc.sh
+++ bashrc.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+falcon_init () {
+    # krew
+    if [ -d ${KREW_ROOT:-$HOME/.krew}/bin ]; then
+    	export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+    fi
+    alias kc1="kubectl --context=cw_condor1"
+    alias kc2="kubectl --context=cw_condor2"
+    alias kcf="kubectl --context=falcon-phx-ga"
+    alias kcfc="kubectl --context=falcon-test-cpu"
+    alias kcfs="kubectl --context=falcon-phx-ga-staging"
+    alias kch="kubectl --context=cw_hawk"
+    alias kcm="kubectl --context=mango-pdx -n mechavarria"
+    alias kc1s="kubectl --context=cw_condor1 -n tenant-slurm-staging"
+    alias kc2s="kubectl --context=cw_condor2 -n tenant-slurm-staging"
+}
+
 abenn_init () {
     pushd "$( dirname "${BASH_SOURCE[0]}" )"
     if [ -f ./tmux_init.sh ]; then
@@ -13,6 +29,8 @@ abenn_init () {
     if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
         export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
     fi
+    alias k="kubectl"
+    falcon_init
 }
 
 kubebuilder_completion () {
